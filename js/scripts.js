9a758bc7d2ef3baa072e3a3a3eecf32e2281a9bf
leekijung <rlwjd4177@naver.com>
2021-04-04 04:09:24 +0900

    Add: base, introduction

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,33 @@
-/*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
-    }
+() => {
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      theme: "light2", // "light1", "light2", "dark1", "dark2"
+      title: {
+        text: "Top Oil Reserves",
+      },
+      axisY: {
+        title: "Reserves(MMbbl)",
+      },
+      data: [
+        {
+          type: "column",
+          showInLegend: true,
+          legendMarkerColor: "grey",
+          legendText: "MMbbl = one million barrels",
+          dataPoints: [
+            { y: 300878, label: "Venezuela" },
+            { y: 266455, label: "Saudi" },
+            { y: 169709, label: "Canada" },
+            { y: 158400, label: "Iran" },
+            { y: 142503, label: "Iraq" },
+            { y: 101500, label: "Kuwait" },
+            { y: 97800, label: "UAE" },
+            { y: 80000, label: "Russia" },
+          ],
+        },
+      ],
+    });
+    chart.render();
   };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
-
-})(jQuery); // End of use strict
+};
