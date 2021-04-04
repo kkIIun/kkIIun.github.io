a5b97f9eb0e71e7111796df22a1cc7c784f0a25f
leekijung <rlwjd4177@naver.com>
2021-04-04 18:49:31 +0900

    Fix: introduction

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,54 @@
-() => {
-  window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
-      animationEnabled: true,
-      theme: "light2", // "light1", "light2", "dark1", "dark2"
-      title: {
-        text: "Top Oil Reserves",
+(() => {
+  let YOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(YOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높의값의 합
+  let currentScene = 0; // 현재 활성화된(뉸 엎애 보고있는) 씬(scroll-section)
+  let enterNewScene = false; // 새로운 scene이 시작된 순간 true
+  let rafstate = false;
+  let rafId;
+  const image = document.getElementById("source");
+  const sceneInfo = [
+    {
+      // 0
+      type: "sticky",
+      heightNum: 1,
+      scrollHeight: 0,
+      objs: {
+        canvas: document.querySelector(".image-blend-canvas"),
+        context: document.querySelector(".image-blend-canvas").getContext("2d"),
       },
-      axisY: {
-        title: "Reserves(MMbbl)",
+      values: {
+        rect1X: [0, 0, { start: 0, end: 1 }],
+        rect2X: [0, 0, { start: 0, end: 1 }],
+        imageBlendHeight: [0, 0, { start: 0, end: 0 }],
+        rectStartY: 0,
+        canvas_scale: [0, 0, { start: 0, end: 0 }],
+        canvasCaption_opacity: [0, 1, { start: 0, end: 0 }],
+        canvasCaption_translateY: [20, 0, { start: 0, end: 0 }],
       },
-      data: [
-        {
-          type: "column",
-          showInLegend: true,
-          legendMarkerColor: "grey",
-          legendText: "MMbbl = one million barrels",
-          dataPoints: [
-            { y: 300878, label: "Venezuela" },
-            { y: 266455, label: "Saudi" },
-            { y: 169709, label: "Canada" },
-            { y: 158400, label: "Iran" },
-            { y: 142503, label: "Iraq" },
-            { y: 101500, label: "Kuwait" },
-            { y: 97800, label: "UAE" },
-            { y: 80000, label: "Russia" },
-          ],
-        },
-      ],
-    });
-    chart.render();
-  };
-};
+    },
+  ];
+
+  function platAnimation() {
+    const widthRatio = window.innerWidth / sceneInfo[0].objs.canvas.width;
+    const heightRatio = window.innerHeight / sceneInfo[0].objs.canvas.height;
+    let canvasScaleRatio;
+
+    if (widthRatio <= heightRatio) {
+      canvasScaleRatio = heightRatio;
+    } else {
+      canvasScaleRatio = widthRatio;
+    }
+
+    sceneInfo[0].objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+  }
+
+  function setCanvas() {
+    platAnimation();
+    sceneInfo[0].objs.context.drawImage(image, 0, 0);
+  }
+
+  image.addEventListener("load", setCanvas);
+
+  window.addEventListener("resize", setCanvas, false);
+  platAnimation();
+})();
