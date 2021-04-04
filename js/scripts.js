a0edfcbc05888e35a9b9b24419a84d446e637935
leekijung <rlwjd4177@naver.com>
2021-04-04 19:51:19 +0900

    Add: Navbar

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,76 @@
       heightNum: 1,
       scrollHeight: 0,
       objs: {
+        container: document.querySelector("#introduction"),
         canvas: document.querySelector(".image-blend-canvas"),
         context: document.querySelector(".image-blend-canvas").getContext("2d"),
       },
-      values: {
-        rect1X: [0, 0, { start: 0, end: 1 }],
-        rect2X: [0, 0, { start: 0, end: 1 }],
-        imageBlendHeight: [0, 0, { start: 0, end: 0 }],
-        rectStartY: 0,
-        canvas_scale: [0, 0, { start: 0, end: 0 }],
-        canvasCaption_opacity: [0, 1, { start: 0, end: 0 }],
-        canvasCaption_translateY: [20, 0, { start: 0, end: 0 }],
+    },
+    {
+      // 1
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#hobby"),
       },
     },
   ];
 
-  function platAnimation() {
+  function calcValues(values, currentYOffset) {
+    let rv;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+    if (values.length === 3) {
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partscrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partscrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+    console.log(scrollRatio);
+    return rv;
+  }
+
+  function scrollLoop() {
+    enterNewScene = false;
+    prevScrollHeight = 0;
+    console.log(YOffset, currentScene);
+
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+    if (YOffset > sceneInfo[currentScene].scrollHeight + prevScrollHeight) {
+      enterNewScene = true;
+      currentScene++;
+    }
+    if (YOffset < prevScrollHeight && YOffset > 0) {
+      enterNewScene = true;
+      currentScene--;
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+
+    if (enterNewScene) return;
+    // playAnimation();
+  }
+
+  function platAnimation() {}
+
+  function setCanvas() {
     const widthRatio = window.innerWidth / sceneInfo[0].objs.canvas.width;
     const heightRatio = window.innerHeight / sceneInfo[0].objs.canvas.height;
     let canvasScaleRatio;
@@ -40,15 +94,33 @@
     }
 
     sceneInfo[0].objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+    sceneInfo[0].objs.context.drawImage(image, 0, 0);
+
+    for (let i = 0; i < sceneInfo.length; i++) {
+      if (sceneInfo[i].type == "sticky") {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      } else if (sceneInfo[i].type == "normal") {
+        console.log(sceneInfo[i].objs.content.offsetHeight);
+        sceneInfo[i].scrollHeight =
+          sceneInfo[i].objs.content.offsetHeight + window.innerHeight * 0.5;
+      }
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
   }
 
-  function setCanvas() {
-    platAnimation();
-    sceneInfo[0].objs.context.drawImage(image, 0, 0);
-  }
+  window.addEventListener("scroll", () => {
+    YOffset = window.pageYOffset;
+    let body = document.querySelector("body");
+    scrollLoop();
+    // if (YOffset > line) {
+    //   body.classList.add("local-nav-sticky");
+    // } else {
+    //   body.classList.remove("local-nav-sticky");
+    // }
+  });
 
   image.addEventListener("load", setCanvas);
-
   window.addEventListener("resize", setCanvas, false);
-  platAnimation();
 })();
